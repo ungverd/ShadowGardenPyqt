80bee17f67726dde03b3e0dbff577e42101f8861
Julia Salnikova (Notiel) <agereth@gmail.com>
2020-01-27 01:39:54 +0300

    refactoring ended, not tested

--- shadoweditor.py
+++ shadoweditor.py
@@ -11,6 +11,7 @@ from typing import List, Dict
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from loguru import logger
+from dataclasses import dataclass, field
 
 import ui
 
@@ -32,11 +33,15 @@ class Usbhost:
         return 500
 
 
-class serial:
+class Serial:
     class Serial(QtWidgets.QWidget):
-        def __init__(self):
+        def __init__(self, port, baudrate, timeout):
             self.i = 0
             self.values = list()
+            self.port = port
+            self.baudrate = baudrate
+            self.timeout = timeout
+            self.previous = ""
 
         def __enter__(self):
             return self
@@ -50,7 +55,6 @@ class serial:
             return res
 # end of emulation
 
-
 class FileFormat(Enum):
     NOT_MUSIC = 1
     CORRECT = 2
@@ -62,6 +66,19 @@ class State(Enum):
     DEST_SELECTED = 2
     SOURCE_SELECTED = 3
     FILES_READY = 4
+    PROCESSING = 5
+
+
+@dataclass
+class MusicProcessor:
+    state: State = State.NOTHING_SELECTED
+    folder_names: List[str] = field(default_factory=list)
+    classify_dict: Dict[str, FileFormat] = field(default_factory=dict())
+    dest: str = ""
+    source: str = ""
+    ser = None
+    csv_file = None
+    current: int = 0
 
 
 class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
@@ -75,27 +92,24 @@ class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
 
     def __init__(self):
         super().__init__()
-        self.state: State = State.NOTHING_SELECTED
+        self.state: MusicProcessor = MusicProcessor()
         self.setupUi(self)
         self.BtnCreateDest.clicked.connect(self.create_dest_folder)
         self.BtnChooseDest.clicked.connect(self.select_folder)
         self.LinePathDest.editingFinished.connect(self.get_folder_from_field)
         self.BtnChooseSource.clicked.connect(self.select_folder)
-        self.BtnConvert.clicked.connect(lambda: self.process_files(self.copy_and_convert))
-        self.BtnSkip.clicked.connect(lambda: self.process_files(self.copyOnly))
+        self.BtnConvert.clicked.connect(self.process_files)
+        self.BtnSkip.clicked.connect(self.process_files)
         self.LinePathSource.editingFinished.connect(self.get_folder_from_field)
+        self.BtnCards.clicked.connect(self.apply_cards_prepare)
+        self.BtnStop.clicked.connect(self.tear_down)
 
-        self.folder_names: List[str] = list()
-        self.dest: str = ""
-        self.source: str = ""
-        self.classify_dict: Dict[str, FileFormat] = dict()
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.timertick)
         self.filesInFolder = QtGui.QStandardItemModel()
         self.LstFiles.setModel(self.filesInFolder)
         self.foldersInFolder = QtGui.QStandardItemModel()
         self.LstDirs.setModel(self.foldersInFolder)
-
-        self.timer = QtCore.QTimer()
-        self.BtnCards.clicked.connect(self.applyCards)
 
     def select_folder(self):
         """
@@ -103,12 +117,17 @@ class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         :return:
         """
         sender = self.sender()
+        if sender == self.BtnChooseDest and self.state.source:
+            reply = QtWidgets.QMessageBox.question(self, 'Message', "Остальные данные будут сброшены, продолжить?",
+                                                   QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
+            if reply == QtWidgets.QMessageBox.No:
+                return
         (ui_function, folder_field) = (self.select_dest_ui, 'dest') if sender == self.BtnChooseDest \
             else (self.select_target_ui_and_convert, 'source')
         title = "Выберите папку"
         folder = QtWidgets.QFileDialog.getExistingDirectory(self, title, os.path.dirname(os.path.abspath(__file__)))
         if folder:
-            setattr(self, folder_field, folder)
+            setattr(self.state, folder_field, folder)
             ui_function()
 
     def create_dest_folder(self):
@@ -117,19 +136,10 @@ class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         If New name is already used, nes folder's name is 'New1" etc
         :return:
         """
-        cur_path = os.getcwd()
-        full_path = os.path.join(cur_path, "new")
-        if not os.path.isdir(full_path):
-            os.mkdir(full_path)
-        else:
-            i = 1
-            new_name = full_path + str(i)
-            while os.path.isdir(new_name):
-                i += 1
-                new_name = full_path + str(i)
-            os.mkdir(new_name)
-            full_path = new_name
-        self.dest = full_path
+        cur_path: str = os.getcwd()
+        full_path: str = os.path.join(cur_path, "new")
+        full_path = create_new_folder(full_path)
+        self.state.dest = full_path
         self.select_dest_ui()
 
     def get_folder_from_field(self):
@@ -137,24 +147,27 @@ class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         writes manually added path to dist/source field and calls corresponding ui function
         """
         sender = self.sender()
+        if sender == self.LinePathDest and self.state.source:
+            reply = QtWidgets.QMessageBox.question(self, 'Message', "Остальные данные будут сброшены, продолжить?",
+                                                   QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
+            if reply == QtWidgets.QMessageBox.No:
+                return
         folder = sender.text()
         ui_function, field_folder = (self.select_dest_ui, 'dest') if sender == self.LinePathDest \
             else (self.select_target_ui_and_convert, 'source')
         if os.path.exists(folder):
             result_path = folder if os.path.isdir(folder) else os.path.split(folder)[0]
-            setattr(self, field_folder,result_path)
+            setattr(self.state, field_folder, result_path)
             ui_function()
         elif sender.text():
-            error_message("Выбранной папки не существует")
+            message_popup("Выбранной папки не существует", 'error')
 
     def select_dest_ui(self):
         """
         makes ui for state "destination folder selected"
         :return:
         """
-        self.source = ""
-        self.classify_dict = dict()
-        self.state = State.DEST_SELECTED
+        self.state = MusicProcessor(state=State.DEST_SELECTED, dest=self.state.dest)
         self.BtnChooseSource.setEnabled(True)
         self.LinePathSource.setEnabled(True)
         self.LinePathSource.clear()
@@ -163,8 +176,9 @@ class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         self.BtnSkip.setEnabled(False)
         self.foldersInFolder.clear()
         self.BtnCards.setEnabled(False)
-        self.LblDest.setText("Выбрана папка: %s" % self.dest)
+        self.LblDest.setText("Выбрана папка: %s" % self.state.dest)
         self.LblSource.setText("Выбрана папка:")
+        self.BtnStop.setEnabled(False)
 
     def insert_item_with_color(self, text: str, color):
         """
@@ -184,11 +198,11 @@ class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         have red colour and we may convert them
         :return:
         """
-        for filename in os.listdir(self.source):
-            src: str = os.path.join(self.source, filename)
+        for filename in os.listdir(self.state.source):
+            src: str = os.path.join(self.state.source, filename)
             try:
                 res = check_file_format(src)
-                self.classify_dict[src] = res
+                self.state.classify_dict[src] = res
                 if res == FileFormat.CORRECT:
                     self.insert_item_with_color(filename, BLACK)
                     self.BtnSkip.setEnabled(True)
@@ -198,17 +212,19 @@ class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
                     self.insert_item_with_color(filename, RED)
                     self.BtnConvert.setEnabled(True)
             except (FileNotFoundError, OSError):
-                error_message("ошибка открытия файла %s" % src)
+                message_popup("Ошибка открытия файла %s" % src, 'error')
                 continue
+        self.state.state = State.SOURCE_SELECTED
 
     def copy_and_convert(self, src: str, dst: str, convert: bool) -> bool:
         """
         convert file from src path to dest path using correct format (wav, 16bit, 48000) or just copy in format is good
+        :param convert: convert files or skip them
         :param src: source file
         :param dst:dest file
         :return: status of operation
         """
-        res = self.classifyDict[src]
+        res = self.state.classify_dict[src]
         if res == FileFormat.good:
             if src != dst:
                 copyfile(src, dst)
@@ -223,82 +239,107 @@ class ShadowUi(QtWidgets.QMainWindow, ui.Ui_MainWindow):
                 return False
         return False
 
-    def process_files(self, func):
+    def process_files(self):
         """
-
-        :param func:
+        creates new folder in dest folder and for every file in selected dest folder copies it, converts or skips
         :return:
         """
-        #get new file name
-        src_path = self.source
-        basename = os.path.basename(src_path)
-        full_dest = os.path.join(self.dest, basename)
-        if not os.path.isdir(full_dest):
-            os.mkdir(full_dest)
-        else:
-            i = 1
-            new_name = full_dest + str(i)
-            while os.path.isdir(new_name):
-                i += 1
-                new_name = full_dest + str(i)
-            os.mkdir(new_name)
-            full_dest = new_name
-            basename = os.path.basename(full_dest)
+        sender = self.sender()
+        full_dest = create_new_folder(os.path.join(self.state.dest, os.path.basename(self.state.source)))
+        basename = os.path.basename(full_dest)
         self.folder_names.append(basename)
         self.foldersInFolder.appendRow(QtGui.QStandardItem(basename))
+        for filename in os.listdir(self.state.source):
+            src_full = os.path.join(self.state.source, filename)
+            dst_filename = os.path.splitext(filename)[0] + '.wav'
+            dst_full = os.path.join(full_dest, dst_filename)
+            must_convert = True if sender == self.BtnConvert else False
+            res = self.copy_and_convert(src_full, dst_full, must_convert)
+            if not res:
+                message_popup('Не удалось конвертировать файл %s' % src_full, 'error')
+        self.state.state = State.FILES_READY
+        self.set_files_ready_ui()
 
-        for filename in os.listdir(src_path):
-            src = os.path.join(src_path, filename)
-            dst_name = filename[:-4] + ".wav"
-            dst = os.path.join(full_dest, dst_name)
-            func(src, dst, True) #возвращает true, если это был подходящий файл, и false, если нет
-
-        self.filesInFolder.clear()
-        self.BtnCards.setEnabled(True)
-        self.BtnConvert.setEnabled(False)
-        self.BtnSkip.setEnabled(False)
+    def apply_cards_prepare(self):
+        """
+        preparation for cards usr
+        :return:
+        """
+        self.color_next_dir(-1)
+        port = Usbhost.get_device_port()
+        self.state.ser = Serial.Serial(port, baudrate=115200, timeout=0.1)
+        self.state.csv_file = open(os.path.join(self.dest, 'folders.csv'), 'w', newline='')
+        self.state.state = State.PROCESSING
+        self.timer.start(1000)
+        self.BtnStop.setEnabled(True)
 
     def timertick(self):
-        try:
-            next(self.gen)
-        except StopIteration:
-            pass
+        """
+        timer tick
+        if we are in processing state, try to read cards
+        :return:
+        """
+        if self.state == State.PROCESSING:
+            writer = csv.writer(self.state.csv_file, dialect='excel')
+            if self.state.current < len(self.state.folder_names):
+                answer = self.ser.readall().decode('utf-8').split('\r')
+                for line in answer:
+                    if line.startswith("Card: "):
+                        if line != self.state.previous and self.state.current < len(self.state.folder_names):
+                            self.state.previous = line
+                            words = line.split()
+                            writer.writerow([words[1], words[2], self.state.folder_names[self.state.current]])
+                            self.color_next_dir(self.state.current)
+                            self.state.current += 1
+            else:
+                message_popup("Запись карточек окончена", "info")
+                self.tear_down()
 
-
-    def applyCards(self):
-        self.color_next_dir(-1)
-        self.gen = self.timertick_gen()
-        self.timer.timeout.connect(self.timertick)
-        self.timer.start(1)
-
-
-    def timertick_gen(self):
-        i = 0
-        port = Usbhost.get_device_port()
-        with Serial.Serial(port, baudrate=115200, timeout=0.1) as self.ser:
-            with open(os.path.join(self.dest, 'folders.csv'), 'w', newline='') as csvfile:
-                writer = csv.writer(csvfile, dialect='excel')
-                previous = ""
-                while i < len(self.folder_names):
-                    answer = self.ser.readall().decode('utf-8').split('\r')
-                    for line in answer:
-                        if line.startswith("Card: ") and line != previous and i < len(self.folder_names):
-                            previous = line
-                            words = line.split(" ")
-                            writer.writerow([words[1], words[2], self.folder_names[i]])
-                            self.color_next_dir(i)
-                            i += 1
-                    yield
+    def tear_down(self):
+        """
+        clears state and closes all files and ports
+        :return:
+        """
         self.timer.stop()
+        self.state.ser.close()
+        self.state.csv_file.close()
+        self.state = MusicProcessor()
+        self.select_dest_ui()
 
-
-    def color_next_dir(self, i):
+    def color_next_dir(self, i: int):
+        """
+        colors current dir in list (if any) back in white
+        colors next dir (if any) in blue
+        :param i:
+        :return:
+        """
         white = QtGui.QBrush(QtGui.QColor(255, 255, 255))
         blue = QtGui.QBrush(QtGui.QColor(200, 200, 255))
         if i >= 0:
             self.foldersInFolder.item(i).setBackground(white)
         if i + 1 < self.foldersInFolder.rowCount():
             self.foldersInFolder.item(i + 1).setBackground(blue)
+
+    def set_files_ready_ui(self):
+        """
+        sets ui for files ready status
+        :return:
+        """
+        self.filesInFolder.clear()
+        self.BtnCards.setEnabled(True)
+        self.BtnConvert.setEnabled(False)
+        self.BtnSkip.setEnabled(False)
+        self.BtnChooseSource.setEnabled(False)
+        self.LinePathSource.setEnabled(False)
+
+    def closeEvent(self, event):
+        """
+        closes all files and ports before closing
+        :param event:
+        :return:
+        """
+        self.tear_down()
+        event.accept()
 
 
 def check_file_format(path):
@@ -321,18 +362,41 @@ def check_file_format(path):
     return FileFormat.NOT_MUSIC
 
 
-def error_message(text):
+def create_new_folder(folder: str) -> str:
+    """
+    creates new folder in path, if folder already exists, creates
+    :param folder: name for folder to create
+    :return: name of really created folder
+    """
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+        return folder
+    i = 1
+    new_name = folder + str(i)
+    while os.path.isdir(new_name):
+        i += 1
+        new_name = folder + str(i)
+    os.mkdir(new_name)
+    return new_name
+
+
+def message_popup(text: str, message_type: str):
     """
     shows error window with text
+    :param message_type: error or info
     :param text: error text
     :return:
     """
-    error = QtWidgets.QMessageBox()
-    error.setIcon(QtWidgets.QMessageBox.Critical)
-    error.setText(text)
-    error.setWindowTitle('Ошибка открытия файла')
-    error.setStandardButtons(QtWidgets.QMessageBox.Ok)
-    error.exec_()
+    message = QtWidgets.QMessageBox()
+    if message_type == 'error':
+        message.setIcon(QtWidgets.QMessageBox.Critical)
+        message.setWindowTitle('Ошибка!')
+    else:
+        message.setIcon(QtWidgets.QMessageBox.Information)
+        message.setWindowTitle('')
+    message.setText(text)
+    message.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    message.exec_()
 
 
 def setup_exception_logging():
